141676030eb1d11e2e2b4ca24faed1d9e5fa768a
kmcknight1 <katiemcknight1991@gmail.com>
2019-08-18 13:22:55 -0500

    adds server, database, users table, authentication, authorization

---
+++ customMiddleware/restricted-middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+//const secrets = require("../secret");
+const secret = process.env.JWT_SECRET || "secret";
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.jwtToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "No token" });
+  }
+};
